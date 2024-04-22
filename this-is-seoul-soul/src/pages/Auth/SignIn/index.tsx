84becdf26e517ea5d935b02570b6707c169bcd15
이채정 <lcj000107@naver.com>
2024-04-22 22:45:10 +0900

    SEOULDATA-101 feat: 구글 소셜 로그인 기능 구현

---
+++ this-is-seoul-soul/src/pages/Auth/SignIn/index.tsx
@@ -0,0 +1,20 @@
+import LogoNoBackground from 'assets/images/LogoNoBackground.png';
+import { cls } from 'utils/cls';
+import { GoogleLoginButton } from 'components/atoms/socialLoginButton/GoogleLoginButton';
+
+export const SignIn = () => {
+  return (
+    <div className='w-full h-full bg-yellow-400 flex justify-center items-center px-4'>
+      <div className='w-full'>
+        <div className={cls('w-56 mb-14 mx-auto')}>
+          <img src={LogoNoBackground} alt='로고' />
+        </div>
+        <div className={cls('w-full py-4 bg-yellow-200')}>
+          <span>
+            <GoogleLoginButton />
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
