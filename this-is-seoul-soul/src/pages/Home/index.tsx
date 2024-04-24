e1a26f09aeac439660949642b9bcbc02bc98a400
KimRiun <kimriun27@gmail.com>
2024-04-24 23:09:09 +0900

    SEOULDATA-58 feat: 지도 검색바 UI 구현
    지도 화면에서의 검색바를 위해 기존 홈 화면에 사용된 검색바에 props로 지도 여부를 받도록 하였습니다. map이라는 boolean 값을 받으므로, 지도 화면에서 SearchBar 컴포넌트 UI를 사용하고 싶으면 `<SearchBar map />`으로 사용하면 됩니다.
    position: fixed나 z-index 요소가 없으므로 실제 지도 구현시 플로팅이 필요하다면 수정해야 할 수 있습니다.

--- this-is-seoul-soul/src/pages/Home/index.tsx
+++ this-is-seoul-soul/src/pages/Home/index.tsx
@@ -2,7 +2,7 @@ import { SearchBar } from "components/organisms/SearchBar";
 
 
 export const HomePage = () => {
-  return <div className="w-full h-full">
+  return <div className="w-full h-full bg-yellow-50">
     <SearchBar />
   </div>;
 };
