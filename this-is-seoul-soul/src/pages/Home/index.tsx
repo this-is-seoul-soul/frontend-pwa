162fc29e22b277f3cce9e51f6b9b354a65745f0d
KimRiun <kimriun27@gmail.com>
2024-04-25 23:56:26 +0900

    SEOULDATA-59 feat: 홈 화면에 축제 아이템 리스트를 grid로 UI 구현
    grid를 이용해 2열로 행이 계속 되게 만들었습니다.
    아직 api 연결과 무한 스크롤 적용되기 전 입니다.
    상단에 축제 아이템 리스트 더미 데이터가 있으므로 api연결하고 지워야 합니다.

--- this-is-seoul-soul/src/pages/Home/index.tsx
+++ this-is-seoul-soul/src/pages/Home/index.tsx
@@ -1,8 +1,120 @@
+import { FestInfoMapItem } from "components/atoms/festInfo/FestInfoMapItem";
+import { FestInfoHomeItem } from "components/atoms/festInfo/FestInfoHomeItem";
 import { SearchBar } from "components/organisms/SearchBar";
+import { cls } from "utils/cls";
 
+const FestDummy = [
+  {
+    festSeq: 1,
+    title: '농어촌 토마토 행사',
+    codeName: '전시/미술',
+    mainImg: '',
+    startDate: '2024/01/12',
+    endDate: '2024/02/10',
+    useFee: '무료',
+    avgPoint: 4.3,
+    cntReview: 66,
+    isContinue: true,
+    isHeart: false,
+  },
+  {
+    festSeq: 1,
+    title: '농어촌 토마토 행사',
+    codeName: '전시/미술',
+    mainImg: '',
+    startDate: '2024/01/12',
+    endDate: '2024/02/10',
+    useFee: '무료',
+    avgPoint: 4.3,
+    cntReview: 66,
+    isContinue: true,
+    isHeart: false,
+  },
+  {
+    festSeq: 1,
+    title: '농어촌 토마토 행사',
+    codeName: '전시/미술',
+    mainImg: '',
+    startDate: '2024/01/12',
+    endDate: '2024/02/10',
+    useFee: '무료',
+    avgPoint: 4.3,
+    cntReview: 66,
+    isContinue: true,
+    isHeart: false,
+  },
+  {
+    festSeq: 1,
+    title: '농어촌 토마토 행사',
+    codeName: '전시/미술',
+    mainImg: '',
+    startDate: '2024/01/12',
+    endDate: '2024/02/10',
+    useFee: '무료',
+    avgPoint: 4.3,
+    cntReview: 66,
+    isContinue: true,
+    isHeart: false,
+  },
+  {
+    festSeq: 1,
+    title: '농어촌 토마토 행사',
+    codeName: '전시/미술',
+    mainImg: '',
+    startDate: '2024/01/12',
+    endDate: '2024/02/10',
+    useFee: '무료',
+    avgPoint: 4.3,
+    cntReview: 66,
+    isContinue: true,
+    isHeart: false,
+  },
+  {
+    festSeq: 1,
+    title: '농어촌 토마토 행사',
+    codeName: '전시/미술',
+    mainImg: '',
+    startDate: '2024/01/12',
+    endDate: '2024/02/10',
+    useFee: '무료',
+    avgPoint: 4.3,
+    cntReview: 66,
+    isContinue: true,
+    isHeart: false,
+  },
+  {
+    festSeq: 1,
+    title: '농어촌 토마토 행사',
+    codeName: '전시/미술',
+    mainImg: '',
+    startDate: '2024/01/12',
+    endDate: '2024/02/10',
+    useFee: '무료',
+    avgPoint: 4.3,
+    cntReview: 66,
+    isContinue: true,
+    isHeart: false,
+  },
+
+];
 
 export const HomePage = () => {
+  const SEARCH_BAR_HEIGHT : number = 52;
+
   return <div className="w-full h-full bg-yellow-50">
-    <SearchBar />
+    <div className={`h-[${SEARCH_BAR_HEIGHT}px]`}>
+      <SearchBar />
+    </div>
+    <div className={`grid grid-cols-2 gap-5 p-4 pb-24 bg-white`}>
+      {FestDummy.length > 0 &&
+        FestDummy.map((fest, index) => (
+          <div key={index}>
+            <FestInfoHomeItem fest={fest} />
+          </div>
+      ))}
+
+
+    </div>
+
   </div>;
 };
