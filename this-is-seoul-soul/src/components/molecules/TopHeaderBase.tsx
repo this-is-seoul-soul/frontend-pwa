f99d19b6ad6a58bc074ff74712b9a586c1999629
KimRiun <kimriun27@gmail.com>
2024-04-28 22:05:58 +0900

    SEOULDATA-110 fix: 상단 헤더 가로 크기의 최대를 루트에 맞게 수정

--- this-is-seoul-soul/src/components/molecules/TopHeaderBase.tsx
+++ this-is-seoul-soul/src/components/molecules/TopHeaderBase.tsx
@@ -10,8 +10,8 @@ export const TopHeaderBase = ({ back = false, title}: BaseTopHeaderProps) => {
     const navigate = useNavigate();
 
     return (
-        <div className="relative h-[47px]">
-            <div className="fixed left-0 right-0  grid grid-cols-3 px-5 py-3 bg-white">
+        <div className="relative  h-[47px]">
+            <div className="fixed max-w-[500px] left-0 right-0 mx-auto grid grid-cols-3 px-5 py-3 bg-white">
                 <div className="justify-self-start">
                     {back ? <div onClick={() => { navigate(-1) }}><IoIosArrowBack size={24} /></div> : <></>}
                 </div>
