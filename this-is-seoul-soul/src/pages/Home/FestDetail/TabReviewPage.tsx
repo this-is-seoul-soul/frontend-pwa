b5ad84809fe96bfd6c472ba20edf6bdf588c2db2
KimRiun <kimriun27@gmail.com>
2024-04-28 22:50:22 +0900

    SEOULDATA-110 feat: 리뷰 탭 이런 점이 좋았어요! 상수 작성
    MVP가 아닌 관계로 일단 해당 section 주석, 다음 작업으로 넘어갑니다

--- this-is-seoul-soul/src/pages/Home/FestDetail/TabReviewPage.tsx
+++ this-is-seoul-soul/src/pages/Home/FestDetail/TabReviewPage.tsx
@@ -9,9 +9,9 @@ interface TabReviewPageProps {
 export const TabReviewPage = ({ fest } : TabReviewPageProps) => {
     return (
         <div>
-            <section className="p-4">
+            {/* <section className="p-4">
                 <TitleSection title="이런 점이 좋았어요!" description={`총 ${fest.cntReview}명 참여`} />
-            </section>
+            </section> */}
             <section className="p-4">
                 <TitleSection title={fest.title} description="다녀오셨나요? 리뷰를 남겨보세요." />
             </section>
