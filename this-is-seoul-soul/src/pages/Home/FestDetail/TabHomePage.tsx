7dc6be3d0da624bcd4e07842c0c81f6318b6c705
KimRiun <kimriun27@gmail.com>
2024-04-28 22:04:07 +0900

    SEOULDATA-110 fix: 축제 상세 페이지의 홈 탭 아이콘과 내용 매칭 수정

--- this-is-seoul-soul/src/pages/Home/FestDetail/TabHomePage.tsx
+++ this-is-seoul-soul/src/pages/Home/FestDetail/TabHomePage.tsx
@@ -15,18 +15,18 @@ export const TabHomePage = ({ fest } : TabHomePageProps) => {
                 <span>{fest.guname} {fest.place}</span>
             </div>
             <div className="flex items-start gap-2">
-            <div><GoPerson size={18} className="mt-1 text-gray-600 stroke-1 stroke-gray-600"/></div>
+            <div><MdAccessTime size={18} className="mt-1 text-gray-600 stroke-1 stroke-gray-600"/></div>
                 <span className="font-bold">{fest.isContinue ? "진행중" : "미진행"}</span>
                 <span id="period" className="text-gray-800">
                     {fest.startDate} ~ {fest.endDate}
                 </span>
             </div>
             <div className="flex items-start gap-2">
-            <div><MdAccessTime size={18} className="mt-1 text-gray-600 stroke-1 stroke-gray-600"/></div>
+            <div><FaWonSign size={18} className="mt-1 text-gray-600 "/></div>
                 <span>{fest.isFree}</span>
             </div>
             <div className="flex items-start gap-2">
-            <div><FaWonSign size={17} className="mt-1 text-gray-600 "/></div>
+            <div><GoPerson size={18} className="mt-1 text-gray-600 stroke-1 stroke-gray-600"/></div>
                 <span>{fest.useTrgt}</span>
             </div>
             <div className="flex items-start gap-2 break-all">
