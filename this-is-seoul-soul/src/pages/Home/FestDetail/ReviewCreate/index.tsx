7e4546843531ac83fd944765b2d6ac6ce974b3cd
KimRiun <kimriun27@gmail.com>
2024-04-29 20:33:03 +0900

    SEOULDATA-120 design: 이미지 첨부 UI 구현

--- this-is-seoul-soul/src/pages/Home/FestDetail/ReviewCreate/index.tsx
+++ this-is-seoul-soul/src/pages/Home/FestDetail/ReviewCreate/index.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { GoStar, GoStarFill } from "react-icons/go";
 import { useLocation, useSearchParams } from "react-router-dom";
+import { MdAddAPhoto } from "react-icons/md";
 
 interface Props {}
 
@@ -33,7 +34,7 @@ export const ReviewCreatePage = ({ }: Props) => {
     for (let i = 1; i <= maxStars; i++) {
         stars.push(
             <span key={i} onClick={() => handleStarClick(i)}>
-                {i <= rating ? <GoStarFill size={52} className="text-yellow-400" /> : <GoStar size={52} className="text-gray-400" />}
+                {i <= rating ? <GoStarFill size={52} className="text-yellow-400" /> : <GoStar size={52} className="text-gray-300" />}
             </span>
         );
     }
@@ -45,6 +46,18 @@ export const ReviewCreatePage = ({ }: Props) => {
                 <div className="flex flex-col items-center gap-2 pt-16">
                     <div className="flex justify-center gap-1">{stars}</div>
                     <div className="text-xl"><span className="font-bold">{rating}</span> / 5</div>
+                </div>
+            </section>
+            <section className="p-8 flex ">
+                <div className="w-[76px] min-w-[76px] h-[76px] flex justify-center items-center mr-1 bg-white border-2 border-yellow-400 rounded-lg">
+                    <MdAddAPhoto size={36} className="text-yellow-400 mr-[4px]" />
+                </div>
+                <div className="flex gap-1 overflow-x-auto whitespace-nowrap scrollbar-hide">
+                    <img className=" w-[76px] h-[76px] object-cover border-2 border-gray-200 rounded-lg" src="https://i.namu.wiki/i/alELF6s0wKly1dUXn2vjdDYFbOjrxITYnTEEHAB-1cNqjwOT56cOT8QTNdzPqWTndhqH2ZkTt-YAkVMmkOveiWfXG8MDhDbzvU5dgUjiDsRdvI-KYLm4_JNO96-PiPevkQsBFJr-XObx1BGfL3COog.webp" alt="" />
+                    <img className=" w-[76px] h-[76px] object-cover border-2 border-gray-200 rounded-lg" src="https://i.namu.wiki/i/alELF6s0wKly1dUXn2vjdDYFbOjrxITYnTEEHAB-1cNqjwOT56cOT8QTNdzPqWTndhqH2ZkTt-YAkVMmkOveiWfXG8MDhDbzvU5dgUjiDsRdvI-KYLm4_JNO96-PiPevkQsBFJr-XObx1BGfL3COog.webp" alt="" />
+                    <img className=" w-[76px] h-[76px] object-cover border-2 border-gray-200 rounded-lg" src="https://i.namu.wiki/i/alELF6s0wKly1dUXn2vjdDYFbOjrxITYnTEEHAB-1cNqjwOT56cOT8QTNdzPqWTndhqH2ZkTt-YAkVMmkOveiWfXG8MDhDbzvU5dgUjiDsRdvI-KYLm4_JNO96-PiPevkQsBFJr-XObx1BGfL3COog.webp" alt="" />
+                    <img className=" w-[76px] h-[76px] object-cover border-2 border-gray-200 rounded-lg" src="https://i.namu.wiki/i/alELF6s0wKly1dUXn2vjdDYFbOjrxITYnTEEHAB-1cNqjwOT56cOT8QTNdzPqWTndhqH2ZkTt-YAkVMmkOveiWfXG8MDhDbzvU5dgUjiDsRdvI-KYLm4_JNO96-PiPevkQsBFJr-XObx1BGfL3COog.webp" alt="" />
+                    <img className="w-[76px] h-[76px] object-cover border-2 border-gray-200 rounded-lg" src="https://img.kbs.co.kr/kbs/620/news.kbs.co.kr/data/fckeditor/new/image/2024/04/01/169771711958787915.jpg" alt="" />               
                 </div>
             </section>
         </div>
