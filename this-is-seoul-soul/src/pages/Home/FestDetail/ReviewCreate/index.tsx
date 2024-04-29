9d6431cf5223f3748340997ec59778bc75bed241
KimRiun <kimriun27@gmail.com>
2024-04-29 19:52:33 +0900

    SEOULDATA-120 design: 별점 등록 UI 구현

--- this-is-seoul-soul/src/pages/Home/FestDetail/ReviewCreate/index.tsx
+++ this-is-seoul-soul/src/pages/Home/FestDetail/ReviewCreate/index.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import { GoStar, GoStarFill } from "react-icons/go";
 import { useLocation, useSearchParams } from "react-router-dom";
 
 interface Props {}
@@ -14,20 +16,37 @@ interface Props {}
 
 export const ReviewCreatePage = ({ }: Props) => {
     const location = useLocation();
-    let { rating } = location.state;
+    // let { rating } = location.state;
     const [searchParams] = useSearchParams();
     const param = searchParams.get('festSeq') || "";
     const festSeq = parseInt(param);
     
+    const [rating, setRating] = useState<number>(location.state.rating);
 
-    console.log(location)
-    console.log(location.state)
+    const handleStarClick = (starNumber: number) => {
+        setRating(starNumber);
+    };
+
+    const maxStars = 5;
+    let stars = [];
+
+    for (let i = 1; i <= maxStars; i++) {
+        stars.push(
+            <span key={i} onClick={() => handleStarClick(i)}>
+                {i <= rating ? <GoStarFill size={52} className="text-yellow-400" /> : <GoStar size={52} className="text-gray-400" />}
+            </span>
+        );
+    }
 
     return (
         <div>
-            리뷰 등록 페이지
-            별점: {rating}
-            festSeq: { festSeq}
+            {/* festSeq: {festSeq} */}
+            <section>
+                <div className="flex flex-col items-center gap-2 pt-16">
+                    <div className="flex justify-center gap-1">{stars}</div>
+                    <div className="text-xl"><span className="font-bold">{rating}</span> / 5</div>
+                </div>
+            </section>
         </div>
     );
 }
