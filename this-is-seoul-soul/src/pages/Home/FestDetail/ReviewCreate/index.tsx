449f366912f987866d422fec5af3d564c631575a
KimRiun <kimriun27@gmail.com>
2024-04-29 21:38:02 +0900

    SEOULDATA-120 design: 리뷰 등록 페이지의 리뷰 입력 텍스트 UI 구현

--- this-is-seoul-soul/src/pages/Home/FestDetail/ReviewCreate/index.tsx
+++ this-is-seoul-soul/src/pages/Home/FestDetail/ReviewCreate/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 import { GoStar, GoStarFill } from "react-icons/go";
 import { useLocation, useSearchParams } from "react-router-dom";
 import { MdAddAPhoto } from "react-icons/md";
@@ -16,6 +16,8 @@ interface Props {}
 */
 
 export const ReviewCreatePage = ({ }: Props) => {
+    const MAX_LENGTH = 1000;
+    let [inputCount, setInputCount] = useState(0);
     const location = useLocation();
     // let { rating } = location.state;
     const [searchParams] = useSearchParams();
@@ -27,6 +29,15 @@ export const ReviewCreatePage = ({ }: Props) => {
     const handleStarClick = (starNumber: number) => {
         setRating(starNumber);
     };
+
+    const handleReviewText = (event:ChangeEvent<HTMLTextAreaElement>) => {
+        const text = event.target.value;
+        if (text.length > MAX_LENGTH) {
+            return
+        }
+        
+        setInputCount(text.length);
+    }
 
     const maxStars = 5;
     let stars = [];
@@ -48,8 +59,8 @@ export const ReviewCreatePage = ({ }: Props) => {
                     <div className="text-xl"><span className="font-bold">{rating}</span> / 5</div>
                 </div>
             </section>
-            <section className="p-8 flex ">
-                <div className="w-[76px] min-w-[76px] h-[76px] flex justify-center items-center mr-1 bg-white border-2 border-yellow-400 rounded-lg">
+            <section className="pt-8 px-8 flex">
+                <div className="w-[76px] min-w-[76px] h-[76px] flex justify-center items-center mr-1 bg-white border border-yellow-400 rounded-lg">
                     <MdAddAPhoto size={36} className="text-yellow-400 mr-[4px]" />
                 </div>
                 <div className="flex gap-1 overflow-x-auto whitespace-nowrap scrollbar-hide">
@@ -58,6 +69,13 @@ export const ReviewCreatePage = ({ }: Props) => {
                     <img className=" w-[76px] h-[76px] object-cover border-2 border-gray-200 rounded-lg" src="https://i.namu.wiki/i/alELF6s0wKly1dUXn2vjdDYFbOjrxITYnTEEHAB-1cNqjwOT56cOT8QTNdzPqWTndhqH2ZkTt-YAkVMmkOveiWfXG8MDhDbzvU5dgUjiDsRdvI-KYLm4_JNO96-PiPevkQsBFJr-XObx1BGfL3COog.webp" alt="" />
                     <img className=" w-[76px] h-[76px] object-cover border-2 border-gray-200 rounded-lg" src="https://i.namu.wiki/i/alELF6s0wKly1dUXn2vjdDYFbOjrxITYnTEEHAB-1cNqjwOT56cOT8QTNdzPqWTndhqH2ZkTt-YAkVMmkOveiWfXG8MDhDbzvU5dgUjiDsRdvI-KYLm4_JNO96-PiPevkQsBFJr-XObx1BGfL3COog.webp" alt="" />
                     <img className="w-[76px] h-[76px] object-cover border-2 border-gray-200 rounded-lg" src="https://img.kbs.co.kr/kbs/620/news.kbs.co.kr/data/fckeditor/new/image/2024/04/01/169771711958787915.jpg" alt="" />               
+                </div>
+            </section>
+            <section className="px-8 py-4">
+                <textarea onChange={handleReviewText} maxLength={MAX_LENGTH} placeholder="리뷰를 작성하세요." className="p-4 w-full h-56 border border-gray-500 rounded-lg focus:outline-none  focus-visible:border-yellow-400 resize-none" />
+                <div className="flex justify-end text-gray-500">
+                    <span>{inputCount}</span>
+                    <span>/{MAX_LENGTH} 자</span>
                 </div>
             </section>
         </div>
