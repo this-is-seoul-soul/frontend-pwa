1eb81a72761a292c8fd6bc3c409318a2df5721cc
jini_11 <jieun21124@naver.com>
2024-04-29 10:20:40 +0900

    feat/SEOULDATA-106 fix: 축제 정보 컴포넌트 css 수정

--- this-is-seoul-soul/src/components/atoms/festInfo/FestInfoMapItem.tsx
+++ this-is-seoul-soul/src/components/atoms/festInfo/FestInfoMapItem.tsx
@@ -6,6 +6,7 @@ import { cls } from 'utils/cls';
 import type { Fest } from 'types/fest';
 import { codeNameColor } from 'constants/codename';
 import { useState } from 'react';
+import { pathname } from 'constants/pathname';
 
 type FestInfoProps = {
   fest: Fest;
@@ -28,6 +29,8 @@ const FestDummy = {
 };
 
 export const FestInfoMapItem = ({ fest }: FestInfoProps) => {
+  const label = (pathname.find((item) => item.path === location.pathname) || {}).label;
+
   fest = fest || FestDummy.fest;
 
   const [isHeart, setIsHeart] = useState(fest.isHeart);
@@ -40,7 +43,11 @@ export const FestInfoMapItem = ({ fest }: FestInfoProps) => {
   };
 
   return (
-    <div className={cls('max-w-full h-[180px] relative rounded-md shadow-lg px-4 py-2 mx-4')}>
+    <div
+      className={cls(
+        `max-w-full relative ${label === '저장' ? '' : 'rounded-md shadow-lg mx-4'} px-4 py-2`
+      )}
+    >
       <div className={cls('w-5 absolute top-5 right-4')} onClick={handleHeart}>
         {isHeart == true ? (
           <img src={BookmarkChecked} alt="즐겨찾기" />
@@ -48,18 +55,20 @@ export const FestInfoMapItem = ({ fest }: FestInfoProps) => {
           <img src={Bookmark} alt="즐겨찾기" />
         )}
       </div>
-      <div className="absolute flex items-center">
-        <img src={image} className={cls('w-36 h-36')} />
-        <div className={cls('p-3')}>
-          <div className={cls(`${codeColor} inline-block rounded-lg text-sm px-2 py-1`)}>
+      <div className="relative flex my-3 items-center">
+        <img src={image} className={cls('w-28 h-28 object-cover')} />
+        <div className={cls('px-3 text-xs')}>
+          <div className={cls(`${codeColor} inline-block rounded-lg px-2 py-1`)}>
             {fest.codeName}
           </div>
-          <div className={cls('font-bold text-lg pt-2')}>{fest.title}</div>
+          <div className={cls('font-bold text-lg pt-2 line-clamp-2')}>
+            {fest.title}
+          </div>
           <div className="py-1">
-            <span className={cls('text-sm text-gray-700 ')}>이용 요금</span>
+            <span className={cls('text-gray-700')}>이용 요금</span>
             <span className={cls('px-3')}>{fest.useFee}</span>
           </div>
-          <div className="relative flex flex-wrap items-center">
+          <div className="relative flex flex-wrap items-center mb-1">
             {fest.isContinue && <span className="pr-3">진행 중</span>}
             <span className={cls('w-3')}>
               <img src={Star} alt="평점" />
@@ -68,7 +77,7 @@ export const FestInfoMapItem = ({ fest }: FestInfoProps) => {
             <span className="px-2">리뷰</span>
             {fest.cntReview >= 50 ? <span>50+</span> : <span>{fest.cntReview}</span>}
           </div>
-          <div className={cls('text-sm text-gray-700')}>
+          <div className={cls('text-gray-700')}>
             {fest.startDate} ~ {fest.endDate}
           </div>
         </div>
