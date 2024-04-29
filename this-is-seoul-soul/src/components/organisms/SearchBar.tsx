bd9c661ecc8d13ad5f2a79169c57b666373069cb
이채정 <lcj000107@naver.com>
2024-04-29 22:13:32 +0900

    feat/SEOULDATA-114 fix: build 에러 해결

--- this-is-seoul-soul/src/components/organisms/SearchBar.tsx
+++ this-is-seoul-soul/src/components/organisms/SearchBar.tsx
@@ -1,28 +1,34 @@
-import { GoSearch } from "react-icons/go";
+import { useAppNavigation } from 'hooks/useAppNavigation';
+import { GoSearch } from 'react-icons/go';
 
 interface SearchBarProps {
-    map?: boolean;
+  map?: boolean;
 }
 
 /* 지도 화면 검색바는 <SearchBar map /> 으로 사용합니다. */
 export const SearchBar = ({ map = false }: SearchBarProps) => {
-    
-    
-    return (
-        <div className={`${map ? "p-4": `h-[52px]`}`}>
-            <div className={`flex w-full max-w-[500px] bg-white shadow-md ${map ? "rounded-xl" : "fixed z-50"}`}>
-                <div className="p-4">
-                    <GoSearch
-                        size={20}
-                        className="text-yellow-400 stroke-1 stroke-yellow-400"
-                    />
-                </div>
-                <input
-                    type="text"
-                    placeholder="어떤 축제를 찾으시나요?"
-                    className=" placeholder-gray-500 w-full mr-4 text-base"
-                />
-            </div>
+  const navigation = useAppNavigation();
+  const handleMoveSearchPage = () => {
+    navigation.navigateToSearch();
+  };
+
+  return (
+    <div className={`${map ? 'px-2 py-1' : `h-[52px]`}`}>
+      <div
+        className={`flex w-full max-w-[500px] bg-white shadow-md ${
+          map ? 'rounded-xl' : 'fixed z-50'
+        }`}
+        onClick={handleMoveSearchPage}
+      >
+        <div className='p-4'>
+          <GoSearch size={20} className='text-yellow-400 stroke-1 stroke-yellow-400' />
         </div>
-    );
-}
+        <input
+          type='text'
+          placeholder='어떤 축제를 찾으시나요?'
+          className=' placeholder-gray-500 w-full mr-4 text-base'
+        />
+      </div>
+    </div>
+  );
+};
--- this-is-seoul-soul/src/pages/Home/FestDetail/index.tsx
+++ this-is-seoul-soul/src/pages/Home/FestDetail/index.tsx
@@ -1,124 +1,128 @@
-import { useSetAtom } from "jotai";
-import { useSearchParams } from "react-router-dom";
-import { headerTitleAtom } from "stores/headerStore";
-import { festDetail } from "types/festDetail";
-import { GoBookmark, GoBookmarkFill, GoShareAndroid, GoStarFill } from "react-icons/go";
-import { useState } from "react";
-import { TabHome } from "../../../components/organisms/festDetail/TabHome";
-import { TabReview } from "../../../components/organisms/festDetail/TabReview";
-import { TabMap } from "../../../components/organisms/festDetail/TabMap";
-import { tabItemType } from "types/tab";
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { useSetAtom } from 'jotai';
+// import { useSearchParams } from 'react-router-dom';
+import { headerTitleAtom } from 'stores/headerStore';
+import { festDetail } from 'types/festDetail';
+import { GoBookmark, GoBookmarkFill, GoShareAndroid, GoStarFill } from 'react-icons/go';
+import { useState } from 'react';
+import { TabHome } from '../../../components/organisms/festDetail/TabHome';
+import { TabReview } from '../../../components/organisms/festDetail/TabReview';
+import { TabMap } from '../../../components/organisms/festDetail/TabMap';
+import { tabItemType } from 'types/tab';
 
-interface Props {}
-
-const fest : festDetail = {
-    festSeq: 12345,
-    title: "오페라 갈라",
-    codename: "뮤지컬/오페라",
-    guname: "종로구",
-    place: "세종대극장",
-    useTrgt: "7세 이상 관람 가능(2017년생부터 관람 가능)",
-    isFree: "R 50,000원 / S 40,000원 / A 30,000원",
-    useFee: "유료",
-    startDate: "2024-11-21",
-    endDate: "2024-11-24",
-    lot: 37.5726241,
-    lat: 126.9760053,
-    orgLink: "https://www.sejongpac.or.kr/portal/performance/performance/view.do?menuNo=200004&performIdx=34877",
-    mainImg: "https://culture.seoul.go.kr/cmmn/file/getImage.do?atchFileId=a6cb4e5a9b5b404e83454b084d88a399&thumb=Y",
-    avgPoint: 4.3,
-    cntReview: 35,
-    isContinue: true,
-    isHeart: true,
-    tag: [
-        {
-            tag: 1,
-            cnt: 23
-        }
-    ]
-}
-
-
+const fest: festDetail = {
+  festSeq: 12345,
+  title: '오페라 갈라',
+  codename: '뮤지컬/오페라',
+  guname: '종로구',
+  place: '세종대극장',
+  useTrgt: '7세 이상 관람 가능(2017년생부터 관람 가능)',
+  isFree: 'R 50,000원 / S 40,000원 / A 30,000원',
+  useFee: '유료',
+  startDate: '2024-11-21',
+  endDate: '2024-11-24',
+  lot: 37.5726241,
+  lat: 126.9760053,
+  orgLink:
+    'https://www.sejongpac.or.kr/portal/performance/performance/view.do?menuNo=200004&performIdx=34877',
+  mainImg:
+    'https://culture.seoul.go.kr/cmmn/file/getImage.do?atchFileId=a6cb4e5a9b5b404e83454b084d88a399&thumb=Y',
+  avgPoint: 4.3,
+  cntReview: 35,
+  isContinue: true,
+  isHeart: true,
+  tag: [
+    {
+      tag: 1,
+      cnt: 23,
+    },
+  ],
+};
 
 const tabs = [
-    { label: '홈', component: <TabHome fest={fest} /> },
-    { label: '리뷰', component: <TabReview fest={fest} /> },
-    { label: '지도', component: <TabMap /> },
+  { label: '홈', component: <TabHome fest={fest} /> },
+  { label: '리뷰', component: <TabReview fest={fest} /> },
+  { label: '지도', component: <TabMap /> },
 ];
 
-export const FestDetailPage = ({ }: Props) => {
-    const [searchParams] = useSearchParams();
-    const param = searchParams.get('festSeq') || "";
-    const festSeq = parseInt(param);
-    
-    const setHeaderTitle = useSetAtom(headerTitleAtom); 
-    setHeaderTitle(fest.title);
+export const FestDetailPage = () => {
+  //   const [searchParams] = useSearchParams();
+  //   const param = searchParams.get('festSeq') || '';
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  //   const festSeq = parseInt(param);
 
-    const [activeTab, setActiveTab] = useState<tabItemType>(tabs[0]);
-    const handleTabClick = (tab: tabItemType) => {
-        setActiveTab(tab);
-    };
+  const setHeaderTitle = useSetAtom(headerTitleAtom);
+  setHeaderTitle(fest.title);
 
-    const [isHeart, setIsHeart] = useState(fest.isHeart);
-    const handleHeart = async (event: React.MouseEvent) => {
-        // TODO: 찜 추가/취소 api 연결
-        setIsHeart(!isHeart);
-    };
+  const [activeTab, setActiveTab] = useState<tabItemType>(tabs[0]);
+  const handleTabClick = (tab: tabItemType) => {
+    setActiveTab(tab);
+  };
 
-    return (
-        <div>
-            <div className="w-full h-32 overflow-hidden">
-                <img src={fest.mainImg} alt="" />
+  const [isHeart, setIsHeart] = useState(fest.isHeart);
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const handleHeart = async (event: React.MouseEvent) => {
+    // TODO: 찜 추가/취소 api 연결
+    console.log(event);
+    setIsHeart(!isHeart);
+  };
+
+  return (
+    <div>
+      <div className='w-full h-32 overflow-hidden'>
+        <img src={fest.mainImg} alt='' />
+      </div>
+      <section>
+        <div className='flex flex-col p-4 pb-8'>
+          <div className='flex justify-end gap-2'>
+            <GoShareAndroid size={24} className=' text-gray-600' />
+            <div onClick={(e) => handleHeart(e)}>
+              {isHeart == true ? (
+                <GoBookmarkFill size={24} className=' fill-yellow-400' />
+              ) : (
+                <GoBookmark size={24} className=' text-gray-400 ' />
+              )}
             </div>
-            <section>
-                <div className="flex flex-col p-4 pb-8">
-                    <div className="flex justify-end gap-2">
-                        <GoShareAndroid size={24} className=" text-gray-600"/>
-                        <div onClick={(e) => handleHeart(e)}>
-                            {isHeart == true ? (
-                                <GoBookmarkFill size={24} className=" fill-yellow-400"/>
-                            ) : (
-                                <GoBookmark size={24} className=" text-gray-400 " />
-                            )}
-                        </div>
-                    </div>
-                    <div className="flex flex-col justify-center items-center">
-                        <div className="text-xl font-extrabold">{fest.title}</div>
-                        <div className="pt-1 text-gray-600">{fest.codename}</div>
-                        <div className="pt-4 flex items-center gap-2">
-                            <div className="flex flex-wrap justify-start items-center gap-4">
-                                <div>{ fest.isContinue ? "진행중" : "미진행"}</div>
-                                <div className="flex items-center gap-1">
-                                    <GoStarFill className="fill-yellow-400" />
-                                    {fest.avgPoint}
-                                </div>
-                                <div className="flex items-center gap-1">
-                                    <div>리뷰</div>
-                                    <div>{fest.cntReview >= 50 ? '50+' : `${fest.cntReview}`}</div>
-                                </div>
-                                    <div className="flex items-center gap-1">
-                                        <GoBookmarkFill className="fill-yellow-400" />
-                                        {fest.tag[0]?.cnt}
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+          </div>
+          <div className='flex flex-col justify-center items-center'>
+            <div className='text-xl font-extrabold'>{fest.title}</div>
+            <div className='pt-1 text-gray-600'>{fest.codename}</div>
+            <div className='pt-4 flex items-center gap-2'>
+              <div className='flex flex-wrap justify-start items-center gap-4'>
+                <div>{fest.isContinue ? '진행중' : '미진행'}</div>
+                <div className='flex items-center gap-1'>
+                  <GoStarFill className='fill-yellow-400' />
+                  {fest.avgPoint}
                 </div>
-            </section>
-            <section>
-                <div className={`grid grid-cols-${tabs.length} border-b-2`}>
-                    {tabs.map((tab, id) => (
-                        <div
-                            key={id}
-                            className={`w-1/2 text-center mx-auto py-2 ${activeTab.label === tab.label ? 'font-extrabold border-b border-b-black' : ''}`}
-                            onClick={() => handleTabClick(tab)}
-                        >
-                            {tab.label}
-                        </div>
-                    ))}
+                <div className='flex items-center gap-1'>
+                  <div>리뷰</div>
+                  <div>{fest.cntReview >= 50 ? '50+' : `${fest.cntReview}`}</div>
                 </div>
-                {activeTab?.component}
-            </section>            
+                <div className='flex items-center gap-1'>
+                  <GoBookmarkFill className='fill-yellow-400' />
+                  {fest.tag[0]?.cnt}
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-    );
-}
+      </section>
+      <section>
+        <div className={`grid grid-cols-${tabs.length} border-b-2`}>
+          {tabs.map((tab, id) => (
+            <div
+              key={id}
+              className={`w-1/2 text-center mx-auto py-2 ${
+                activeTab.label === tab.label ? 'font-extrabold border-b border-b-black' : ''
+              }`}
+              onClick={() => handleTabClick(tab)}
+            >
+              {tab.label}
+            </div>
+          ))}
+        </div>
+        {activeTab?.component}
+      </section>
+    </div>
+  );
+};
