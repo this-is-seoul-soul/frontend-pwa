fc5f2bc50ece254f8962bfccdba13f586442f8a7
KimRiun <kimriun27@gmail.com>
2024-05-02 02:20:35 +0900

    SEOULDATA-113 feat: 리뷰 등록 페이지에 사진 추가할 수 있게 구현

--- this-is-seoul-soul/src/pages/Home/FestDetail/ReviewCreate/index.tsx
+++ this-is-seoul-soul/src/pages/Home/FestDetail/ReviewCreate/index.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, useEffect, useState } from 'react';
+import { ChangeEvent, useState } from 'react';
 import { GoStar, GoStarFill } from 'react-icons/go';
 import { useLocation, useSearchParams } from 'react-router-dom';
 import { MdAddAPhoto } from 'react-icons/md';
@@ -7,7 +7,6 @@ import { TitleSection } from 'components/atoms/titles/TitleReviewSection';
 import { ReviewCreateGoodList } from 'components/molecules/ReviewCreateGoodList/ReviewCreateGoodList';
 import { BottomButton2 } from 'components/atoms/buttons/BottomButton2';
 import { useForm, FormProvider } from 'react-hook-form';
-import { goodList } from 'constants/review';
 
 /* Request Dto
 {
@@ -22,32 +21,28 @@ import { goodList } from 'constants/review';
 interface FormData {
   content: string;
   point: number;
+  imgUrl: Array<string>;
   tag: Array<number>;
 }
 
 export const ReviewCreatePage = () => {
-  const navigation = useAppNavigation();
   const MAX_LENGTH = 10;
-  // let [inputCount, setInputCount] = useState(0);
+  const STARS = [1, 2, 3, 4, 5];
+  const navigation = useAppNavigation();
   const location = useLocation();
   const [searchParams] = useSearchParams();
   const param = searchParams.get('festSeq') || '';
   const festSeq = parseInt(param);
 
-  // const [rating, setRating] = useState<number>(location.state.rating);
   const methods = useForm<FormData>({
     defaultValues: {
       point: location.state.rating,
       content: '',
+      imgUrl: [],
       tag: [],
     },
   });
   const { register, handleSubmit, setValue, watch } = methods;
-
-  // const handleStarClick = (starNumber: number) => {
-  //   // setRating(starNumber);
-  //   setValue('point', starNumber);
-  // };
 
   const handleContent = (event: ChangeEvent<HTMLTextAreaElement>) => {
     const text = event.target.value;
@@ -55,12 +50,21 @@ export const ReviewCreatePage = () => {
       return;
     }
     setValue('content', text);
-    // setInputCount(text.length);
   };
 
-  useEffect(() => {
-    console.log(watch());
-  }, [watch()]);
+  const [imgUrls, setImgUrls] = useState<string[]>([]);
+  const handleImageUpload = (event: ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files) {
+      const file = event.target.files[0];
+      const updatedImgUrls = imgUrls.concat(URL.createObjectURL(file));
+      setImgUrls(updatedImgUrls);
+      setValue('imgUrl', [...watch('imgUrl'), file.name]);
+    }
+  };
+
+  // useEffect(() => {
+  //   console.log(watch());
+  // }, [watch()]);
 
   const onSubmit = (data: any) => {
     // TODO: 리뷰 등록 api 연결
@@ -68,15 +72,13 @@ export const ReviewCreatePage = () => {
     navigation.navigateToFestDetail(festSeq);
   };
 
-  const starArr = [1, 2, 3, 4, 5];
-
   return (
     <FormProvider {...methods}>
       <form onSubmit={handleSubmit(onSubmit)}>
         <section>
           <div className="flex flex-col items-center gap-2 pt-16">
             <div className="flex justify-center gap-1">
-              {starArr.map((starNum) => (
+              {STARS.map((starNum) => (
                 <span key={starNum} onClick={() => setValue('point', starNum)}>
                   {starNum <= watch('point') ? (
                     <GoStarFill size={52} className="text-yellow-400" />
@@ -94,34 +96,28 @@ export const ReviewCreatePage = () => {
 
         <section className="pt-8 px-8 flex">
           <div className="w-[76px] min-w-[76px] h-[76px] flex justify-center items-center mr-1 bg-white border border-yellow-400 rounded-lg">
-            <MdAddAPhoto size={36} className="text-yellow-400 mr-[4px]" />
+            <label htmlFor="photo">
+              <MdAddAPhoto size={36} className="text-yellow-400 mr-[4px]" />
+            </label>
+            <input
+              id="photo"
+              type="file"
+              accept="image/*"
+              onChange={handleImageUpload}
+              className="hidden"
+            />
           </div>
-          <div className="flex gap-1 overflow-x-auto whitespace-nowrap scrollbar-hide">
-            <img
-              className=" w-[76px] h-[76px] object-cover border-2 border-gray-200 rounded-lg"
-              src="https://i.namu.wiki/i/alELF6s0wKly1dUXn2vjdDYFbOjrxITYnTEEHAB-1cNqjwOT56cOT8QTNdzPqWTndhqH2ZkTt-YAkVMmkOveiWfXG8MDhDbzvU5dgUjiDsRdvI-KYLm4_JNO96-PiPevkQsBFJr-XObx1BGfL3COog.webp"
-              alt=""
-            />
-            <img
-              className=" w-[76px] h-[76px] object-cover border-2 border-gray-200 rounded-lg"
-              src="https://i.namu.wiki/i/alELF6s0wKly1dUXn2vjdDYFbOjrxITYnTEEHAB-1cNqjwOT56cOT8QTNdzPqWTndhqH2ZkTt-YAkVMmkOveiWfXG8MDhDbzvU5dgUjiDsRdvI-KYLm4_JNO96-PiPevkQsBFJr-XObx1BGfL3COog.webp"
-              alt=""
-            />
-            <img
-              className=" w-[76px] h-[76px] object-cover border-2 border-gray-200 rounded-lg"
-              src="https://i.namu.wiki/i/alELF6s0wKly1dUXn2vjdDYFbOjrxITYnTEEHAB-1cNqjwOT56cOT8QTNdzPqWTndhqH2ZkTt-YAkVMmkOveiWfXG8MDhDbzvU5dgUjiDsRdvI-KYLm4_JNO96-PiPevkQsBFJr-XObx1BGfL3COog.webp"
-              alt=""
-            />
-            <img
-              className=" w-[76px] h-[76px] object-cover border-2 border-gray-200 rounded-lg"
-              src="https://i.namu.wiki/i/alELF6s0wKly1dUXn2vjdDYFbOjrxITYnTEEHAB-1cNqjwOT56cOT8QTNdzPqWTndhqH2ZkTt-YAkVMmkOveiWfXG8MDhDbzvU5dgUjiDsRdvI-KYLm4_JNO96-PiPevkQsBFJr-XObx1BGfL3COog.webp"
-              alt=""
-            />
-            <img
-              className="w-[76px] h-[76px] object-cover border-2 border-gray-200 rounded-lg"
-              src="https://img.kbs.co.kr/kbs/620/news.kbs.co.kr/data/fckeditor/new/image/2024/04/01/169771711958787915.jpg"
-              alt=""
-            />
+          <div className="flex flex-row-reverse gap-1 overflow-x-auto whitespace-nowrap scrollbar-hide">
+            {imgUrls.map((item, idx) => {
+              return (
+                <img
+                  key={idx}
+                  className=" w-[76px] h-[76px] object-cover border-2 border-gray-200 rounded-lg"
+                  src={item}
+                  alt=""
+                />
+              );
+            })}
           </div>
         </section>
 
