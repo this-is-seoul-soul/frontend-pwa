17868d3f9757219895a41fa15f62271075514bdb
이채정 <lcj000107@naver.com>
2024-05-08 10:21:02 +0900

    feat/SEOULDATA-122 feat: refresh token 재발급 설정

--- this-is-seoul-soul/src/apis/index.ts
+++ this-is-seoul-soul/src/apis/index.ts
@@ -1,5 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import Axios from 'axios';
+import { refreshTokenApi } from './userApi';
+import { debounce } from 'utils/debounce';
 
 const axios = Axios.create({
   baseURL: `/api`,
@@ -18,6 +20,32 @@ axios.interceptors.request.use(
   },
   (err) => {
     return Promise.reject(err);
+  }
+);
+
+axios.interceptors.response.use(
+  (res) => {
+    return res;
+  },
+  (err) => {
+    console.log('response error', err);
+    if (err.response.status === 403) {
+      const refresh = async () => {
+        const res = await refreshTokenApi();
+        if (res.status === 200) {
+          axios.defaults.headers.common[`Authorization`] = res.data.data.accessToken;
+          if (typeof window !== 'undefined') {
+            localStorage.setItem('accessToken', res.data.data.accessToken);
+          }
+          console.log('갱신중');
+        }
+      };
+      if (typeof window !== 'undefined') {
+        const debounceFunction = debounce(refresh(), 1000);
+        debounceFunction();
+      }
+    }
+    return err;
   }
 );
 
