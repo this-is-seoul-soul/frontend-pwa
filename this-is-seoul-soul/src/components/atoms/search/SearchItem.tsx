ceec1c3da9a503b7a1eca58a6b8b7ca9af362af8
KimRiun <kimriun27@gmail.com>
2024-05-10 00:11:45 +0900

    SEOULDATA-143 design: 검색어 입력 중인 경우 UI 수정

--- this-is-seoul-soul/src/components/atoms/search/SearchItem.tsx
+++ this-is-seoul-soul/src/components/atoms/search/SearchItem.tsx
@@ -1,3 +1,7 @@
-export const SearchItem = () => {
-  return <div>searchItem</div>;
+interface SearchItemProps {
+  keyword: string;
+}
+
+export const SearchItem = ({ keyword }: SearchItemProps) => {
+  return <div>검색어 '{keyword}' api 요청 결과...</div>;
 };
