15574b7c1a8b1b7b7ddf877e3be47daacb8853d8
KimRiun <kimriun27@gmail.com>
2024-05-10 00:25:31 +0900

    SEOULDATA-143 feat: 검색 결과 페이지의 검색바 생성

--- this-is-seoul-soul/src/pages/Search/SearchResult/index.tsx
+++ this-is-seoul-soul/src/pages/Search/SearchResult/index.tsx
@@ -1,5 +1,29 @@
+import { IoIosArrowBack } from 'react-icons/io';
+import { useNavigate, useSearchParams } from 'react-router-dom';
+
 interface Props {}
 
 export const SearchResultPage = ({}: Props) => {
-  return <div>검색 결과 화면</div>;
+  const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
+  const keyword = searchParams.get('keyword') || '';
+
+  return (
+    <div className='bg-white flex h-[6%] px-4 pt-1 gap-5 items-center shadow-md'>
+      <div
+        onClick={() => {
+          navigate(-1);
+        }}
+      >
+        <IoIosArrowBack size={24} className='text-gray-800' />
+      </div>
+
+      <input
+        type='text'
+        value={keyword}
+        onFocus={() => navigate(-1)}
+        className=' placeholder-gray-500 w-full mr-4 text-base'
+      />
+    </div>
+  );
 };
