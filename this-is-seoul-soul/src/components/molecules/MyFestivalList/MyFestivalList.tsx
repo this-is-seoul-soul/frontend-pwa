899a3a0c06a4327ed7bb7c0b2e9132aa829dbfbe
jini_11 <jieun21124@naver.com>
2024-05-12 00:40:26 +0900

    feat/SEOULDATA-159 feat: 내가 작성한 정보 관련 api 연결

--- this-is-seoul-soul/src/components/molecules/MyFestivalList/MyFestivalList.tsx
+++ this-is-seoul-soul/src/components/molecules/MyFestivalList/MyFestivalList.tsx
@@ -1,12 +1,35 @@
+import { myFestInfoApi } from 'apis/festApi';
+import { FestInfoMapItem } from 'components/atoms/festInfo/FestInfoMapItem';
 import { useAppNavigation } from 'hooks/useAppNavigation';
+import { useEffect, useState } from 'react';
 import { FaAngleRight } from 'react-icons/fa6';
+import { FestType } from 'types/fest';
 
 export const MyFestivalList = () => {
+  const [festivals, setFestivals] = useState<FestType[]>([]);
   const navigation = useAppNavigation();
 
   const handleMoveFestivalRegistration = () => {
     navigation.navigateToFestivalRegister();
   };
+
+  useEffect(() => {
+    const fetchFestivals = async () => {
+      try {
+        const res = await myFestInfoApi();
+
+        if (res.status === 200) {
+          console.log('내 축제 조회 성공');
+          setFestivals(res.data.data);
+        }
+      } catch (error) {
+        console.error('Failed to fetch festivals:', error);
+      }
+    };
+
+    fetchFestivals();
+  }, []);
+
   return (
     <div>
       <div className='flex justify-between items-center py-4'>
@@ -18,6 +41,11 @@ export const MyFestivalList = () => {
           <div>등록하기</div>
           <FaAngleRight />
         </div>
+      </div>
+      <div>
+        {festivals.map((fest) => (
+          <FestInfoMapItem fest={fest} />
+        ))}
       </div>
     </div>
   );
