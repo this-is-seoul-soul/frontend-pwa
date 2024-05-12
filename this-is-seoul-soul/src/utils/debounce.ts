9e8850c8bbb68938e5c3c0b8d4193a3b8811862d
이채정 <lcj000107@naver.com>
2024-05-13 01:01:01 +0900

    feat/SEOULDATA-133 feat: 검색 처리

--- this-is-seoul-soul/src/utils/debounce.ts
+++ this-is-seoul-soul/src/utils/debounce.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+let timeoutId: any;
+
 export function debounce(fn: any, delay: number) {
-  let timeoutId: any;
   return (...args: any) => {
     clearTimeout(timeoutId);
     timeoutId = setTimeout(() => fn(...args), delay);
