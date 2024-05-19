a2339cc89fb007e010331728d032b5dbdc3fe774
KimRiun <kimriun27@gmail.com>
2024-05-19 21:55:10 +0900

    SEOULDATA-81 feat: 무한 스크롤되는지 api 붙여서 테스트

---
+++ this-is-seoul-soul/src/hooks/useInfiniteScroll.tsx
@@ -0,0 +1,56 @@
+import { AxiosResponse } from 'axios';
+import { useCallback, useEffect, useRef, useState } from 'react';
+
+interface useInfiniteScrollProps<T, P> {
+  fetchFn: (params: P) => Promise<AxiosResponse>;
+  initialParams: P;
+  getNextParams: (currentParams: P) => P;
+  hasMoreItems: (response: T[]) => boolean;
+}
+
+export const useInfiniteScroll = <T, P>({
+  fetchFn,
+  initialParams,
+  getNextParams,
+  hasMoreItems,
+}: useInfiniteScrollProps<T, P>) => {
+  const [items, setItems] = useState<T[]>([]);
+  const [hasMore, setHasMore] = useState<boolean>(true);
+  const [params, setParams] = useState<P>(initialParams);
+  const loaderRef = useRef<HTMLDivElement | null>(null); // 사용하는 컴포넌트에서 반드시 써야함
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      try {
+        const response = await fetchFn(params);
+        const data = (await response.data) as T[];
+        setItems((prev) => [...prev, ...data]);
+        setHasMore(hasMoreItems(data));
+      } catch (error) {
+        console.error('Failed to fetch items:', error);
+      }
+    };
+
+    fetchItems();
+  }, [fetchFn, params]);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting && hasMore) {
+        setParams((prevParams) => getNextParams(prevParams));
+      }
+    });
+
+    if (loaderRef.current) {
+      observer.observe(loaderRef.current);
+    }
+
+    return () => {
+      if (loaderRef.current) {
+        observer.unobserve(loaderRef.current);
+      }
+    };
+  }, [hasMore, getNextParams]);
+
+  return { items, loaderRef, hasMore };
+};
